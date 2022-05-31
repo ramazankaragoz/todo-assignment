dbd8fc021b624b9918dff8a0fa49377f9c3f5d17
ramazan.karagoz <ramazan.karagoz@netas.com.tr>
2022-05-31 21:56:40 +0300

    Initial commit

---
+++ group-api/src/main/java/com/comodo/groupapi/GroupApiApplication.java
@@ -0,0 +1,13 @@
+package com.comodo.groupapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupApiApplication.class, args);
+    }
+
+}
