5b638223d3a495084143cf41df38c4e29b6505e9
ramazan.karagoz <2wHY[]BnM{Wf>
2022-06-02 17:56:54 +0300

    fixed h2 console remote connection problem

--- user-api/src/main/java/com/comodo/userapi/config/WebSecurityConfig.java
+++ user-api/src/main/java/com/comodo/userapi/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.comodo.userapi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,6 +12,12 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class WebSecurityConfig{
+
+    @Bean
+    WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/webjars/**","/h2-console/**");
+    }
+
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
